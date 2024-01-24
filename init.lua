d613fa48f44cae5927527ab92abfcdf53965ec68
hanstar17 <hanstar17@gmail.com>
2024-01-24 00:03:10 -0800

    tokionight theme and mason plugins

---
+++ init.lua
@@ -0,0 +1,30 @@
+-- Prepare lazy.nvim script
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Key mappings
+vim.g.mapleader = " "
+
+require("lazy").setup({
+	{
+		"folke/tokyonight.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- require("tokionight").setup({})
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+})
